ffede704c528d74d4093c6f1f9f3f749c88db4ab
Ricardo Carvalho <rscarvalho@hotmail.com>
2020-09-13 16:21:20 -0300

    Teste novo commit

--- src/Teste.java
+++ src/Teste.java
@@ -1,6 +1,7 @@
 public class Teste {
 
     public static void main(String[] args) {
-        System.out.println("Olá, tudo ?");
+        System.out.println("Olá, tudo bem?");
+        System.out.println("Vamos estudar?");
     }
 }
